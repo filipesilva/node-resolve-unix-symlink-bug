a14da7433b1a73a9e5a95856994ea6a46d443cce
Filipe Silva <filipematossilva@gmail.com>
2017-03-31 22:38:17 +0100

    ignore THIS_IS_UNDEFINED warnings

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,12 @@ export default {
   dest: 'build.js',
   sourceMap: false,
   format: 'iife',
+  onwarn: function (warning) {
+    // Skip certain warnings
+    if (warning.code === 'THIS_IS_UNDEFINED') { return; }
+    // console.warn everything else
+    console.warn(warning.message);
+  },
   plugins: [
     nodeResolve({ jsnext: true, module: true }),
     commonjs({
