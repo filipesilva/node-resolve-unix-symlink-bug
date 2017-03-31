11d3d35a4571fcc7823bb4a174a997bce9ff1634
Filipe Silva <filipematossilva@gmail.com>
2017-03-31 22:07:40 +0100

    rollup in the base dir as well

--- rollup.config.js
+++ rollup.config.js
@@ -7,12 +7,6 @@ export default {
   dest: 'build.js',
   sourceMap: false,
   format: 'iife',
-  onwarn: function (warning) {
-    // Skip certain warnings
-    if (warning.code === 'THIS_IS_UNDEFINED') { return; }
-    // console.warn everything else
-    console.warn(warning.message);
-  },
   plugins: [
     nodeResolve({ jsnext: true, module: true }),
     commonjs({
